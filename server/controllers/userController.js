c22b02173af49b2e295cf82f39e68b9ccbe02db2
Jean de Dieu <jeandedieuam@gmail.com>
2019-01-13 14:21:45 -0800

    update the file userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,9 +3,7 @@ const uuid= require("uuid");
 // Models
 
 const User= require("../models/userModel");
-const Rsvp = require("../models/rsvpModel");
-const Question = require("../models/questionModel");
-const Meetup = require("../models/meetupModel");
+
 
 module.exports= {
 
