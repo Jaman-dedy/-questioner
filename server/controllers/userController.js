885e08706e09e510888ca362e726b579ad8e44d8
Jean de Dieu <jeandedieuam@gmail.com>
2019-01-13 14:21:03 -0800

    set userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,10 @@ const uuid= require("uuid");
 // Models
 
 const User= require("../models/userModel");
+const Rsvp = require("../models/rsvpModel");
+const Question = require("../models/questionModel");
+const Meetup = require("../models/meetupModel");
 
 module.exports= {
-    
+
 }
