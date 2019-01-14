831a5fe433c47c0276f6d285f88bc82465539aa6
Jean de Dieu <jeandedieuam@gmail.com>
2019-01-13 19:54:18 -0800

    create a new user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,5 +5,20 @@ const uuid= require("uuid");
 const User= require("../models/userModel");
 
 module.exports= {
+    create:(req, res)=>{
+     const newUser={
+     id:uuid(),
+     firstname:req.body.firstname,
+     lastname:req.body.lastname,
+     othername:req.body.othername,
+     email:req.body.email,
+     phoneNumber:req.body.phoneNumber,
+     username:req.body.username,
+     registered:req.body.registered,
+     isAdmin:req.body.isAdmin
+     };
+     User.createNewUser(newUser);
+     return res.json(newUser);
+    }
 
 }
